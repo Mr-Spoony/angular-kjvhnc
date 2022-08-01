9b8fbfe32c7af0f5360fbf170003b6b6452317da
Mr-Spoony <alexander.i.littlewood@gmail.com>
2022-08-02 11:22:10 +1200

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,5 @@ Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at https://angular.io/license
 
-Add a new hero
+Delete a new hero
 */
